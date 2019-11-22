c388738b2993210358f624a555d6500c0d122984
Joseph Davis <joseph.davis@hrblock.com>
2019-11-22 08:14:57 -0600

    Clean up files

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const multer = require('multer');
 
 const app = express();
 const upload = multer();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
@@ -24,5 +25,5 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
